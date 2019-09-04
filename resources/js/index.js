89b534924e3de19bb4b3d0ee21febcc392a7ab48
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-09-03 21:01:00 -0400

    Working on adding remove button functionality and button styling
    I've added a bit of styling to the remove and backlog buttons, but it's rough. I also have worked on adding functionality to the remove button, but so far no luck. No added funcionality yet.
    
    Changes to be committed:
    	modified:   resources/css/index.css
    	modified:   resources/js/index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,27 @@
 let addButton = document.getElementById('todayAdd');
 let todayList = document.getElementById('todayList');
 
+const rowStyle = "margin: 1em; display: flex; justify-content: space-between; align-items: center; width: 100%; height: 2em;";
+
+let numRows = 0;
+
+const RemoveButton = {
+    style: "background-color: red;",
+    content: "Remove",
+}
+
+const BacklogButton = {
+    style: "background-color: yellow;",
+    content: "Backlog",
+}
+
 function createEntry() {
     /*Creating new row at last position of table*/
     let newRow = todayList.insertRow(-1);
 
     /*Styling new row*/
-    newRow.style="margin: 1em; display: flex; justify-content: space-between; width: 100%; max-width: 800px";
-    newRow.class="entry";
+    newRow.style = rowStyle;
+    newRow.class = "entry";
 
     /*Inserting four cells in that row*/
     let checkbox = newRow.insertCell(0);
@@ -15,17 +29,25 @@ function createEntry() {
     let remove = newRow.insertCell(2);
     let backlog = newRow.insertCell(3);
 
-    /*Setting classes of new cells*/
-    checkbox.class="checkbox";
-    content.class="entryContent";
-    remove.class="remove button";
-    backlog.class="backlog button";
-
     /*Adding text to cells in new row (placeholder for when I take in content)*/
-    checkbox.innerHTML = "New Checkbox";
-    content.innerHTML = "New Entry";
-    remove.innerHTML = "New Remove";
-    backlog.innerHTML = "New Backlog";
+    checkbox.innerHTML = '<input type="checkbox">';
+    content.innerHTML = toString(numRows);
+    remove.innerHTML = RemoveButton.content;
+    backlog.innerHTML = BacklogButton.content;
+
+    /*Adding styling to content, as well as remove and backlog buttons*/
+    remove.style = RemoveButton.style;
+    backlog.style = BacklogButton.style;
+
+    /*Adding ID to remove button to be used later*/
+    remove.id = numRows;
+
+    /*Adding functionality to buttons*/
+    remove.onclick = document.todayList.deleteRow(newRow.id);
+    backlog.onclick = BacklogButton.moveRow;
+
+    /*Incrementing the number of rows by 1*/
+    numRows += 1;
 }
 
 addButton.onclick = createEntry;
